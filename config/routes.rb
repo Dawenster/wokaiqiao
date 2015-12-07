9b6426c49775517a26dbc2b3bb6157e14c272090
David Wen <dstwen@gmail.com>
2015-12-06 16:33:19 -0800

    Setup static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get "experts" => "experts#index", as: :experts
 
   post "book" => "bookings#create", as: :book
+
+  get "feedback" => "pages#feedback", as: :feedback
+  get "terms" => "pages#terms", as: :terms
 end
